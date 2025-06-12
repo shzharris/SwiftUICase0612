88ff1f89e54988c424def8664e56f818cc6b3dc0
Harris.Mac <shaohaizhou36@gmail.com>
2025-06-12 08:49:35 +0800

    Initial Commit

---
+++ GeneralSetting/GeneralSettingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeneralSettingApp.swift
+//  GeneralSetting
+//
+//  Created by HarrisShao on 2025/6/12.
+//
+
+import SwiftUI
+
+@main
+struct GeneralSettingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
